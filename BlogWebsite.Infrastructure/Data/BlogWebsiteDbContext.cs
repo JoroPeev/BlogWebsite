5d6c822f0bc22b1652660335bbf9cb2dbb260e46
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-07-02 22:42:33 +0300

    Adding skeleton of project

---
+++ BlogWebsite.Infrastructure/Data/BlogWebsiteDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebsite.Infrastructure.Data
+{
+    public class BlogWebsiteDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public BlogWebsiteDbContext(DbContextOptions<BlogWebsiteDbContext> optons):base(optons)
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
