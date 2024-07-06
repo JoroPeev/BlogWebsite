892904e871b55726ae2a47bcf3197c244cdb55b5
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-07-06 14:53:58 +0300

    Adding constants

---
+++ BlogWebsite.Infrastructure/Data/Constants/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebsite.Infrastructure.Data.Constants
+{
+    public static class Constants
+    {
+        public const int FirstNameMax = 20;
+        public const int LastNameMax = 30;
+        public const int RaitingMax = 5;
+    }
+}
